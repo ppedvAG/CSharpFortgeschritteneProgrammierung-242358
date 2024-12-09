441d8134da3c5c06a07e1f79aa126766cda7d5c7
ppedvLukasK <lukask@ppedv.de>
2024-12-09 14:38:30 +0100

    M003

---
+++ Generic/Constraints.cs
@@ -0,0 +1,42 @@
+﻿namespace Generic;
+
+public class Constraints
+{
+	public void TestVererbung()
+	{
+		IEnumerable<Lebewesen> lw = new List<Mensch>();
+		//lw.Add(new Mensch());
+	}
+
+	public T Test<T>()// where T : new()
+		where T : class
+	{
+		//return new T(); //Was ist, wenn T keinen Standardkonstruktor hat?
+
+		T t = null;
+		return t;
+	}
+}
+
+public class DataStore1<T> where T : struct; //T muss ein Wertetyp sein
+
+public class DataStore2<T> where T : class; //T muss ein Wertetyp sein
+
+public class DataStore3<T> where T : new(); //T muss einen Standardkonstruktor haben
+
+public class DataStore4<T> where T : Enum; //T muss ein Enumtyp sein
+
+public class DataStore5<T> where T : Delegate; //T muss ein Delegatetyp sein
+
+public class DataStore6<T> where T : unmanaged; //T muss ein Basisdatentyp oder ein Pointertyp sein
+
+public class DataStore7<T> where T : notnull; //T darf nicht Nullable sein
+
+public class DataStore8<T1, T2>
+	where T1 : class, new() //Mehrere Constrains mit Komma getrennt
+	where T2 : struct; //Mehrere Constraints auf mehrere Generics
+
+
+public class Lebewesen;
+
+public class Mensch : Lebewesen;
---
+++ Generic/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+
+namespace Generic;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		//Generic: Platzhalter für einen konkreten Typen
+		//Wird innerhalb der Klasse/Methode ausgetauscht durch den gegebenen Typen
+
+		//Beispiel: List<T>
+		List<int> ints = []; //Überall in der List Klasse, wird T durch int ersetzt
+		ints.Add(10); //Add(T item) -> Add(int item)
+
+		List<string> strings = [];
+		strings.Add("A"); //Add(T item) -> Add(string item)
+
+		Dictionary<string, int> dict = [];
+		dict.Add("B", 2);
+    }
+
+	static void Test<T>()
+	{
+
+	}
+}
+
+public class DataStore<T> : IEnumerable<T> //T bei Vererbung
+{
+	private T[] _data { get; set; } //T bei Feld
+
+	public List<T> Data => _data.ToList(); //T bei einem anderen Generic
+
+	public void Add(T item, int index) //T als Parameter
+	{
+		_data[index] = item;
+	}
+
+	public IEnumerator<T> GetEnumerator() => throw new NotImplementedException();
+
+	IEnumerator IEnumerable.GetEnumerator() => throw new NotImplementedException();
+
+	public T Get(int index) => _data[index]; //T bei Rückgabewert
+
+	////////////////////////////////////////////////
+	
+	public void Test()
+	{
+        Console.WriteLine(default(T)); //Standardwert von T
+        Console.WriteLine(nameof(T)); //Stringrepräsentation von T
+        Console.WriteLine(typeof(T)); //Typ hinter T
+    }
+}
